60dcea9ae9afd98146aaff15637eabf047f73c1d
Nitin <nitin8285824551@gmail.com>
2022-04-27 00:45:52 +0530

    JavaScript console.log

---
+++ log.js
@@ -0,0 +1,13 @@
+// Example 1: print a sentence
+// passing string
+console.log("I love JS");
+
+
+// Example 2: Print Values Stored in Variables
+// program to print variables values
+
+// storing values
+const greet = 'Hello';
+const name = 'Jack';
+
+console.log(greet + ' ' + name);
