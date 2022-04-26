9993413f068ebd2b50ba6eaeea68ab644414445e
Nitin <nitin8285824551@gmail.com>
2022-04-27 00:47:50 +0530

    JavaScript Comments

---
+++ comments.js
@@ -0,0 +1,21 @@
+// JavaScript Comments
+
+/*
+    // - Single line Comments
+    /* * / - Multi line Comments
+*/
+
+name = "Jack";
+
+// printing name on the console
+console.log("Hello " + name);
+
+// Multi line comment
+/* The following program contains the source code for a game called Baghchal. 
+
+Baghchal is a popular board game in Nepal where two players choose either sheep or tiger. It is played on a 5x5 grid.
+
+For the player controlling the tiger to win, they must capture all the sheep. There are altogether 4 tigers on the board.
+
+For the sheep to win, all tigers must be surrounded and cornered so that they cannot move. The player controlling the sheep has 20 sheep at his disposal.
+*/
