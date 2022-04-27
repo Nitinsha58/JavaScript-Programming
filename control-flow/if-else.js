8c82b0249ea97e87aa40c4a2723c87256477a17a
Nitin <nitin8285824551@gmail.com>
2022-04-27 10:57:04 +0530

    JavaScript if else

---
+++ control-flow/if-else.js
@@ -0,0 +1,14 @@
+// JavaScript if else statement
+
+// Example 1: if statement
+// check if the number is positive
+
+const number = 34;
+
+// check if number is greater than 0
+if (number > 0) {
+    // the body of the if statement
+    console.log("The number is positive");
+}else if (number === 0) {
+    console.log("The number is negative")
+}
