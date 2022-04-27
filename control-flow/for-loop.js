8a0d3bd34c64fcb0710985dde879ba8c7602d2d6
Nitin <nitin8285824551@gmail.com>
2022-04-27 10:57:18 +0530

    JavaScript for loop

---
+++ control-flow/for-loop.js
@@ -0,0 +1,21 @@
+// JavaScript for loop
+
+// Example 1: Display a Text Five Times
+// Program to display text 5 times
+
+for (let i = 1; i <= 5; i++){
+    console.log("I like gumnaam")
+}
+
+// Example 2: Displya numbers from 1 to 5
+for (let i = 1; i <= 5; i++){
+    console.log(i)
+}
+
+// Example 3: Display Sum of n Natural Numbers
+let sum = 0;
+let n = 5;
+for (let i = 1; i <= n; i++){
+    sum += i;
+}
+console.log(`The Sum is ${sum}`)
