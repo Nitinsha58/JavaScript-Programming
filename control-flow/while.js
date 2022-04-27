baf2ede18b439358f34c98b718a1306e8992f89d
Nitin <nitin8285824551@gmail.com>
2022-04-27 10:57:32 +0530

    JavaScript while loop

---
+++ control-flow/while.js
@@ -0,0 +1,20 @@
+// JavaScript while and do while loop
+
+// // Example 1: Display numbers from 1 to 5
+// let i = 1;
+// while (i <= 5) {
+//     console.log(i)
+//     i++
+// }
+
+
+// JavaScript Do while loop
+// Example 3: Display numbers from 1 to 5
+let j = 1; 
+const n = 5;
+
+
+do {
+    console.log(j);
+    j++;
+} while (j <= n);
