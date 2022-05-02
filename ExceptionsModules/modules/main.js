423df7fef56463971b326701abaa3e102e81c3ab
Nitin <nitin8285824551@gmail.com>
2022-05-03 00:40:52 +0530

    JavaScript modules

---
+++ ExceptionsModules/modules/main.js
@@ -0,0 +1,5 @@
+// // Renaming import inside module.js
+
+// // import { newName1, newname2 } from "./modules.js";
+
+// import random_name from './modules.js'
---
+++ ExceptionsModules/modules/modules.js
@@ -0,0 +1,16 @@
+// JavaScript Modules
+
+
+// // Renaming in the module (export file)
+// export {
+//     function1 as newName1,
+//     function2 as newName2
+// };
+
+
+// Default Export
+export default function gree(name) {
+    return `hello ${name}`;
+}
+
+export const age = 23;
